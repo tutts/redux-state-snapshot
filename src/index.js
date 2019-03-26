c6597921450b86c1a8bd0ea34340b485b9595561
Sam Tutton <sam@quiqup.com>
2019-03-26 13:27:38 +0000

    add tests and fix implementation

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import configureStore from 'redux-mock-store'
 
-// const actions = store.getActions()
-//       actionsToStateSnapshot(actions)
-
-/*
-import { actionsToStateSnapshot } from 'redux-state-snapshot'
-
-const actionStateSnapshot = actionsToStateSnapshot(middlewares)
-actionStateSnapshot(getUser, userReducer, {})
-
-*/
-
-export const actionsToStateSnapshot = (middlewares, initialState) => (action, reducer, state = undefined) => {
+export const actionsToStateSnapshot = (middlewares, initialState) => (action, reducer) => {
   const store = createStore(middlewares, initialState)
-  return runActions(store, action, reducer, state)
+
+  return runActions(store, action, reducer, initialState)
 }
 
 const createStore = (middlewares, initialState) => {
@@ -25,8 +15,9 @@ const createStore = (middlewares, initialState) => {
 
 const runActions = async (store, action, reducer, state = undefined) => {
   await store.dispatch(action())
+  const actions = store.getActions()
 
-  for (const action in store.getActions()) {
+  for (const action in actions) {
     const newState = reducer(state, actions[action])
     expect(newState).toMatchSnapshot(actions[action].type)
   }
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,90 @@
-describe('GIVEN as user', () => {
+import thunk from 'redux-thunk'
+
+import { actionsToStateSnapshot } from './index'
+
+const ACTION_TYPE_TEST = 'ACTION_TYPE_TEST'
+
+function starting() {
+  return {
+    type: `${ACTION_TYPE_TEST}_STARTING`,
+  }
+}
+
+function success(payload) {
+  return {
+    type: `${ACTION_TYPE_TEST}_SUCCESS`,
+    payload,
+  }
+}
+
+function failure(payload) {
+  return {
+    type: `${ACTION_TYPE_TEST}_FAILURE`,
+    payload,
+  }
+}
+
+function fetchDataSuccess() {
+  return dispatch => {
+    return Promise.resolve()
+      .then(dispatch(starting()))
+      .then(() => dispatch(success({ error: null, data: ['foo', 'bar'] })))
+  }
+}
+
+function fetchDataFailure() {
+  return dispatch => {
+    return Promise.resolve()
+      .then(dispatch(starting()))
+      .then(() => dispatch(failure({ error: 'Uh oh!', data: [] })))
+  }
+}
+
+function doAction(foo) {
+  return { type: ACTION_TYPE_TEST, foo }
+}
+
+const reducer = (state = { data: [], error: null, loading: false }, action) => {
+  switch (action.type) {
+    case ACTION_TYPE_TEST:
+      return { ...state, foo: action.foo }
+    case `${ACTION_TYPE_TEST}_STARTING`:
+      return { ...state, loading: true }
+    case `${ACTION_TYPE_TEST}_FAILURE`:
+    case `${ACTION_TYPE_TEST}_SUCCESS`:
+      return { data: action.payload.data, error: action.payload.error, loading: false }
+    default:
+      return state
+  }
+}
+
+describe('GIVEN a user', () => {
   describe('WHEN I want to test my async redux actions and state', () => {
-    test('THEN I can use actionsToStateSnapshot', () => {
-      expect(true).toEqual(true)
+    describe('THEN I can use actionsToStateSnapshot for async actions', () => {
+      test('to snapshot on success', () => {
+        const actionStateSnapshot = actionsToStateSnapshot([thunk])
+        actionStateSnapshot(() => fetchDataSuccess(), reducer)
+      })
+
+      test('to snapshot on failure', () => {
+        const actionStateSnapshot = actionsToStateSnapshot([thunk])
+        actionStateSnapshot(() => fetchDataFailure(), reducer)
+      })
+    })
+
+    describe('AND I can use actionsToStateSnapshot for sync actions', () => {
+      test('to snapshot on success', () => {
+        const actionStateSnapshot = actionsToStateSnapshot()
+        actionStateSnapshot(() => doAction('bar'), reducer)
+      })
+    })
+
+    describe('AND I can use actionsToStateSnapshot with a default state', () => {
+      test('to snapshot on success', () => {
+        const initialState = { customData: true }
+        const actionStateSnapshot = actionsToStateSnapshot([], initialState)
+        actionStateSnapshot(() => doAction('bar'), reducer)
+      })
     })
   })
 })
